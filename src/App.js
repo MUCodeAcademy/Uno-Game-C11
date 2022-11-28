3bdb60635722fc3dc7b5ce0f7386dc34e3a33f53
Stevenbmay <sbmay019@gmail.com>
2022-11-28 14:56:06 -0600

    added protected routes and boilerplate to GameRoom

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
 import "./App.css";
+import GamePage from "./components/Game/GamePage";
+import LobbyPage from "./components/Lobby/LobbyPage";
+import LoginPage from "./components/Login/LoginPage";
 
 function App() {
-  return <div>APP WORKS</div>;
+  const { user, clearUser } = useUserContext()
+  return (
+    <div>
+      {auth.currentUser && (
+        <>
+          <div>{auth.currentUser?.displayName}</div>
+          <button onClick={() => {
+            clearUser
+            auth.signOut()
+          }}>Sigh Out</button>
+        </>
+      )}
+      <Router>
+        <Routes>
+          <Route path="/login" element={!user ? <LoginPage /> : <Navigate to="/Lobby" />} />
+          <Route path='/Lobby' element={user ? <LobbyPage /> : <Navigate to="/login" />} />
+          <Route path='/GameRoom/:id' element={user ? <GamePage /> : <Navigate to="/login" />} />
+          <Route path="*" element={<Navigate to="/login" />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
