670f856b1e7de213d80f373f2bfc2a520006c9b7
Stevenbmay <sbmay019@gmail.com>
2022-11-28 14:07:52 -0600

    started waiting room

--- src/components/Game/components/WaitingRoom.jsx
+++ src/components/Game/components/WaitingRoom.jsx
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
 export function WaitingRoom() {
-  return <div>WaitingRoom</div>;
+  const [ishost, setIsHost] = useState(true)
+
+  function Waiting() {
+    if (ishost === true) {
+      return (
+        <div>
+          <div>Press Start When Ready.</div>
+          <button onClick={() => {
+            //set game to start
+          }}>Start</button>
+        </div>
+      )
+    }
+    if (ishost === false) {
+      return (
+        <div>Waiting for host to start the game.</div>
+      )
+    }
+  }
+
+
+  return (
+    <div>
+      <div>chatRoom here</div>
+      <div><Waiting /></div>
+    </div>)
 }
 
 export default WaitingRoom;
