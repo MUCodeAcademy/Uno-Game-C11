b369d211df9c2452714c596df8517693018c9795
Tom Heafey <tmheafey@gmail.com>
2022-11-28 16:31:41 -0600

    adds deal cards function for multiplayer

---
+++ src/shared/functions/dealCards.js
@@ -0,0 +1,11 @@
+export default function dealCards(deck, playerCount) {
+    let hands = Array(playerCount).fill([]);
+
+    for (const hand of hands) {
+        for (i = 0; i < 7; i++) {
+            hand.push(deck.pop());
+        }
+    }
+
+    return { deck, hands };
+}
