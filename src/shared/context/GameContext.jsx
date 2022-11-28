7599dbbae03a786e3a652fc32651656304349a5d
gabebierman <gabebierman@gmail.com>
2022-11-28 15:15:11 -0600

    fix user context , fix game context temp implementation of fireauth and protected routes in app.js

--- src/shared/context/GameContext.jsx
+++ src/shared/context/GameContext.jsx
@@ -1,28 +1,29 @@
-import React, {
-    useReducer,
-    useContext,
-    useCallback,
-} from "react";
+import React, { useReducer, useContext, useCallback } from "react";
 import { createContext } from "react";
-import { gameReducer, INITIAL_ACTIVE_GAME_STATE, SET_ACTIVE_GAME } from "../reducers/GameReducer"
+import {
+    gameReducer,
+    INITIAL_ACTIVE_GAME_STATE,
+    SET_ACTIVE_GAME,
+} from "../reducers/GameReducer";
 
-const GameContext = createContext(null)
+const GameContext = createContext(null);
 
 export function useGameConText() {
-    return useContext(GameContext)
+    return useContext(GameContext);
 }
 
 export function GameProvider(props) {
-    const [activeGame, dispatch] = useReducer(gameReducer, INITIAL_ACTIVE_GAME_STATE)
+    const [activeGame, dispatch] = useReducer(gameReducer, INITIAL_ACTIVE_GAME_STATE);
 
     const setActiveGame = useCallback(
-        (activeGame) => { dispatch({ type: SET_ACTIVE_GAME, payload: activeGame }) },
+        (activeGame) => {
+            dispatch({ type: SET_ACTIVE_GAME, payload: activeGame });
+        },
         [dispatch]
-    )
+    );
     return (
         <GameContext.Provider value={{ activeGame, setActiveGame }}>
             {props.children}
         </GameContext.Provider>
-    )
-
+    );
 }
