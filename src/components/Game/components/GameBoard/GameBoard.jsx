2a5533e3bc8a74f32c07eeb9318b83d5e059d8cb
gabebierman <gabebierman@gmail.com>
2022-11-29 16:37:12 -0600

    clean up excess , remove files

--- src/components/Game/components/GameBoard/GameBoard.jsx
+++ src/components/Game/components/GameBoard/GameBoard.jsx
@@ -1,24 +1,6 @@
-import React, { useState, useEffect } from "react";
-import buildDeck from "../../../../shared/functions/buildDeck";
-import dealCards from "../../../../shared/functions/dealCards";
-import initialCard from "../../../../shared/functions/initialCard";
-import shuffleDeck from "../../../../shared/functions/shuffleDeck";
-import validateGameCard from "../../../../shared/functions/validateGameCard";
+import React from "react";
 
 function GameBoard() {
-    const playerCount = 2;
-    const [deck, setDeck] = useState(shuffleDeck(buildDeck()));
-    const { deck: playDeck, hands } = dealCards(deck, playerCount);
-    const [hand, setHand] = useState(hands[0]);
-    const hand2 = hands[1];
-    const [gameCard, setGameCard] = useState({});
-    const discard = [];
-    function discardCard(card) {
-        discard.push(card);
-    }
-
-    discardCard(gameCard);
-
     return <div>GameBoard</div>;
 }
 
