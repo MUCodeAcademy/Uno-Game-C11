d3e85b588a900f1340ffc01589097cb876489199
mark waring~ <markwaring00@gmail.com>
2022-12-01 14:48:45 -0600

    adds start game socket message

--- src/shared/hooks/useSocket.js
+++ src/shared/hooks/useSocket.js
@@ -3,65 +3,71 @@ import { Navigate, useNavigate } from "react-router-dom";
 import io from "socket.io-client";
 
 const useSocketHook = (roomID, username) => {
-    const socketRef = useRef(null)
-    const [messages, setMessages] = useState([])
-    const [started, setStarted] = useState(false)
-    const [cards, setCards] = useState({})
-    useEffect(() => {
-        socketRef.current = io("http://localhost:8080", {
-            query: {
-                username,
-                roomID,
-            },
-        })
+  const socketRef = useRef(null);
+  const [messages, setMessages] = useState([]);
+  const [gameActive, setGameActive] = useState(false);
+  const [cards, setCards] = useState({});
+  const [players, setPlayers] = useState([]);
+  useEffect(() => {
+    socketRef.current = io("http://localhost:8080", {
+      query: {
+        username,
+        roomID,
+      },
+    });
 
-        socketRef.current.on("user join", (username) => {
-            setMessages((curr) => [...curr, { body: `${username} has connected` }]);
-        });
+    socketRef.current.on("user join", (username) => {
+      setMessages((curr) => [...curr, { body: `${username} has connected` }]);
+    });
 
-        socketRef.current.on("new message", (msg) => {
-            setMessages((curr) => [...curr, msg])
-        })
+    socketRef.current.on("new message", (msg) => {
+      setMessages((curr) => [...curr, msg]);
+    });
 
+    socketRef.current.on("user left", (username) => {
+      setMessages((curr) => [...curr, { body: `${username} has connected` }]);
+    });
 
-        socketRef.current.on("user left", (username) => {
-            setMessages((curr) => [...curr, { body: `${username} has connected` }])
-        })
+    socketRef.current.on("start game", () => {
+      setGameActive(true);
+    });
 
-        socketRef.current.on("start game", () => {
-            setStarted(true)
-        })
+    socketRef.current.on("send cards", (cards) => {
+      setCards(cards);
+    });
 
-        socketRef.current.on("send cards", (cards) => {
-            setCards(cards)
-        })
+    socketRef.current.on("end trun", () => {
+      //don't know yet
+    });
 
-        socketRef.current.on("end trun", () => {
-            //don't know yet
-        })
+    return () => socketRef.current?.disconnect();
+  }, [roomID, username]);
 
+  function sendMessage(body) {
+    socketRef.current.emit("new message", { body });
+  }
 
-        return () => socketRef.current?.disconnect()
+  function sendStart() {
+    socketRef.current.emit("start game");
+  }
 
-    }, [roomID, username]);
+  function endTurn() {
+    socketRef.current.emit("end turn");
+  }
 
-    function sendMessage(body) {
-        socketRef.current.emit("new message", { body })
-    }
+  function sendCards(cards) {
+    socketRef.current.emit("send cards", { cards });
+  }
 
-    function sendStart() {
-        socketRef.current.emit("start game")
-    }
-
-    function endTurn() {
-        socketRef.current.emit("end turn")
-    }
-
-    function sendCards(cards) {
-        socketRef.current.emit("send cards", { cards })
-    }
-
-    return { messages, sendMessage, started, sendStart, endTurn, sendCards, cards };
+  return {
+    messages,
+    sendMessage,
+    started,
+    sendStart,
+    endTurn,
+    sendCards,
+    cards,
+  };
 };
 
 export default useSocketHook;
