02f8135b93d2f15c00c893f7d197d27958dd6ca7
mark waring~ <markwaring00@gmail.com>
2022-12-02 12:05:12 -0600

    implements game start socket

--- src/shared/functions/newGame.js
+++ src/shared/functions/newGame.js
@@ -2,31 +2,34 @@ import buildDeck from "./buildDeck";
 import dealCards from "./dealCards";
 import shuffleDeck from "./shuffleDeck";
 import { CardColor, CardValue } from "./cardEnums";
+import useGameSocketHook from "../../../../shared/hooks/useGameSocket";
+const { started, sendStart, endTurn, sendCards, cards, drawCard } =
+  useGameSocketHook();
 
 //assuming players looks something like this
 
 function newGame(players) {
-    const deck = shuffleDeck(buildDeck());
-    let gameStartCard = null;
-    let { deck: newDeck, hands } = dealCards(deck, players.length);
+  const deck = shuffleDeck(buildDeck());
+  let gameStartCard = null;
+  let { deck: newDeck, hands } = dealCards(deck, players.length);
 
-    for (let i = 0; i < players.length; i++) {
-        players[i].hand = hands[i];
+  for (let i = 0; i < players.length; i++) {
+    players[i].hand = hands[i];
+  }
+
+  for (let i = newDeck.length - 1; i >= 0; i--) {
+    if (
+      newDeck[i].color !== CardColor.Black &&
+      newDeck[i].value !== CardValue.Skip &&
+      newDeck[i].value !== CardValue.Reverse &&
+      newDeck[i].value !== CardValue.DrawTwo
+    ) {
+      gameStartCard = newDeck.splice(i, 1)[0];
+      break;
     }
+  }
 
-    for (let i = newDeck.length - 1; i >= 0; i--) {
-        if (
-            newDeck[i].color !== CardColor.Black &&
-            newDeck[i].value !== CardValue.Skip &&
-            newDeck[i].value !== CardValue.Reverse &&
-            newDeck[i].value !== CardValue.DrawTwo
-        ) {
-            gameStartCard = newDeck.splice(i, 1)[0];
-            break;
-        }
-    }
-
-    return { newDeck, players, gameStartCard };
+  return { newDeck, players, gameStartCard };
 }
 
 export default newGame;
