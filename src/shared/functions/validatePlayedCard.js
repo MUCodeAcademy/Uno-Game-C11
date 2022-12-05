44269507eb880046bc84a368a3d11fd9739acd57
gabebierman <gabebierman@gmail.com>
2022-12-05 14:34:48 -0600

    remove some console log

--- src/shared/functions/validatePlayedCard.js
+++ src/shared/functions/validatePlayedCard.js
@@ -1,23 +1,20 @@
 import { CardColor } from "./cardEnums";
 
 export function validatePlayedCard(playedCard, activeCard) {
-  console.log(activeCard);
-  console.log(playedCard);
-  let allow = false;
-  //if wild, allow
-  if (playedCard.color === CardColor.Black) {
-    allow = true;
-  } else {
-    //if color matches, allow
-    if (playedCard.color === activeCard.color) {
-      allow = true;
-    } else {
-      //if color doesn't match but value does, allow
-      if (playedCard.value === activeCard.value) {
+    let allow = false;
+    //if wild, allow
+    if (playedCard.color === CardColor.Black) {
         allow = true;
-      }
+    } else {
+        //if color matches, allow
+        if (playedCard.color === activeCard.color) {
+            allow = true;
+        } else {
+            //if color doesn't match but value does, allow
+            if (playedCard.value === activeCard.value) {
+                allow = true;
+            }
+        }
     }
-  }
-  console.log(allow);
-  return allow;
+    return allow;
 }
