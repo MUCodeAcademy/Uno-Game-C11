b682a41c5f97cb590fc61b4fa1c8ad9d7b8eeaa0
gabebierman <gabebierman@gmail.com>
2022-12-05 09:32:10 -0600

    Fix imports and typos

--- src/shared/functions/newGame.js
+++ src/shared/functions/newGame.js
@@ -1,36 +1,37 @@
 import buildDeck from "./buildDeck";
 import dealCards from "./dealCards";
 import shuffleDeck from "./shuffleDeck";
+import { CardValue, CardColor } from "./cardEnums";
 
 //assuming players looks something like this
 const dummyPlayers = [
-  { name: "player1", hand: [], isTurn: false },
-  { name: "player2", hand: [], isTurn: false },
+    { name: "player1", hand: [], isTurn: false },
+    { name: "player2", hand: [], isTurn: false },
 ];
 
 export function newGame(players) {
-  // const { started, sendStart, endTurn, sendCards, cards, drawCard } = useSocketHook();
-  const deck = shuffleDeck(buildDeck());
-  const gameStartCard = null;
-  let { deck: newDeck, hands } = dealCards(deck, players.length);
+    // const { started, sendStart, endTurn, sendCards, cards, drawCard } = useSocketHook();
+    const deck = shuffleDeck(buildDeck());
+    const gameStartCard = null;
+    let { deck: newDeck, hands } = dealCards(deck, players.length);
 
-  for (let i = 0; i < players.length; i++) {
-    players[i].hand = hands[i];
-  }
-
-  for (let i = newDeck.length - 1; i >= 0; i--) {
-    if (
-      newDeck[i].color !== CardColor.Black &&
-      newDeck[i].value !== CardValue.Skip &&
-      newDeck[i].value !== CardValue.Reverse &&
-      newDeck[i].value !== CardValue.DrawTwo
-    ) {
-      gameStartCard = newDeck.splice(i, 1)[0];
-      break;
+    for (let i = 0; i < players.length; i++) {
+        players[i].hand = hands[i];
     }
-  }
 
-  return { newDeck, players, gameStartCard };
+    for (let i = newDeck.length - 1; i >= 0; i--) {
+        if (
+            newDeck[i].color !== CardColor.Black &&
+            newDeck[i].value !== CardValue.Skip &&
+            newDeck[i].value !== CardValue.Reverse &&
+            newDeck[i].value !== CardValue.DrawTwo
+        ) {
+            gameStartCard = newDeck.splice(i, 1)[0];
+            break;
+        }
+    }
+
+    return { newDeck, players, gameStartCard };
 }
 
 export default newGame;
