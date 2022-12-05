ff84bfa710f8ef0f86f7e9794a0d91c380a9b9f1
Tom Heafey <tmheafey@gmail.com>
2022-12-05 13:08:20 -0600

    implements card component into player hand

--- src/components/Game/components/GameBoard/components/PlayerHand.jsx
+++ src/components/Game/components/GameBoard/components/PlayerHand.jsx
@@ -1,34 +1,14 @@
 import React, { useEffect, useMemo, useRef, useState } from "react";
 import { useGameContext } from "../../../../../shared/context/GameContext";
-import {
-    removeCardFromHand,
-    validatePlayedCard,
-    playCard,
-    shuffleDeck,
-    CardValue,
-    CardColor,
-    reshuffleDeck,
-} from "../../../../../shared/functions";
+import { removeCardFromHand, validatePlayedCard, playCard, shuffleDeck, CardValue, CardColor, reshuffleDeck } from "../../../../../shared/functions";
 import ChooseColorPrompt from "./ChooseColorPrompt";
 import { auth } from "../../../../../firebase.config";
 import { Button } from "../../../../../shared/styled/components/Button";
 import { theme } from "../../../../../shared/styled/themes/Theme";
 import Card from "./Card";
 
-
 function PlayerHand({ endTurn, drawCard, endGame, reshuffle }) {
-    const {
-        players,
-        activeCard,
-        setActiveCard,
-        isGameActive,
-        playDeck,
-        discardDeck,
-        setShuffling,
-        isReverse,
-        discardDeck,
-        turn,
-    } = useGameContext();
+    const { players, activeCard, setActiveCard, isGameActive, playDeck, setShuffling, isReverse, discardDeck, turn } = useGameContext();
     const [playedWild, setPlayedWild] = useState(false);
     //! this wasn't working because playerhand is rerendering and setting these back to undefined
     const newPlayers = useRef();
@@ -41,6 +21,7 @@ function PlayerHand({ endTurn, drawCard, endGame, reshuffle }) {
     });
 
     let playerIndex = players.findIndex((p) => p.uid === auth.currentUser.uid);
+
     function handleDrawClick() {
         //only allow draw/playcard when it's current player's turn (and they aren't currently picking a color after playing a wild)
         if (turn === playerIndex && !playedWild) {
@@ -60,20 +41,12 @@ function PlayerHand({ endTurn, drawCard, endGame, reshuffle }) {
             if (validatePlayedCard(card, activeCard)) {
                 newPlayers.current = removeCardFromHand(players, playerIndex, card);
                 newActiveCard.current = card;
-                newIsReverse.current =
-                    card.value === CardValue.Reverse ? !isReverse : isReverse;
+                newIsReverse.current = card.value === CardValue.Reverse ? !isReverse : isReverse;
                 newDiscardDeck.current = [...discardDeck, card];
                 setPlayedWild(card.color === CardColor.Black);
                 //if wild played, wait for color picker prompt before ending turn
                 if (card.color !== CardColor.Black) {
-                    endTurn(
-                        newPlayers.current,
-                        newDiscardDeck.current,
-                        newActiveCard.current,
-                        newIsReverse.current,
-                        turn,
-                        playDeck
-                    );
+                    endTurn(newPlayers.current, newDiscardDeck.current, newActiveCard.current, newIsReverse.current, turn, playDeck);
                 }
             }
         }
@@ -88,7 +61,6 @@ function PlayerHand({ endTurn, drawCard, endGame, reshuffle }) {
                 //shuffle needs to
                 reshuffle(reshuffleDeck(playDeck, discardDeck, activeCard));
                 //  setPlayDeck(shuffleDeck(discardDeck))
-
             } else {
                 //TODO: handle case when there is no discard deck (meaning all players have drawn all available cards)
                 endGame(false, "Stalemate");
@@ -142,36 +114,11 @@ function PlayerHand({ endTurn, drawCard, endGame, reshuffle }) {
                 />
             )}
             <div style={{ display: "flex" }}>
-                {isGameActive &&
-                    players[playerIndex] &&
-                    players[playerIndex].hand.map((card, idx) => (
-                        <div
-                            style={{
-                                height: "50px",
-                                width: "50px",
-                                margin: "0px 0px 0px 50px",
-                            }}
-                            value={card}
-                            onClick={() => handlePlayCardClick(card)}
-                            key={idx}
-                        >
-                            <img
-                                src={require(`./cards/${card?.color}_${card?.value}.png`)}
-                                style={{ maxHeight: "200px" }}
-                            ></img>
-                        </div>
-                    ))}
-
-
+                {isGameActive && players[playerIndex] && players[playerIndex].hand.map((card, idx) => <Card key={idx} isTurn={isPlayersTurn} card={card} handlePlayCardClick={handlePlayCardClick} />)}
             </div>
-            <div style={{ margin: "150px 0px 0px 0px" }}>
-                {turn === playerIndex && (
-                    <h4 style={{ color: theme.palette.secondary.main }}>It's your turn!</h4>
-                )}
-            </div>
+            <div style={{ margin: "150px 0px 0px 0px" }}>{isPlayersTurn && <h4 style={{ color: theme.palette.secondary.main }}>It's your turn!</h4>}</div>
             <div>
                 <Button onClick={() => handleDrawClick()}>Draw Card</Button>
-
             </div>
         </div>
     );
