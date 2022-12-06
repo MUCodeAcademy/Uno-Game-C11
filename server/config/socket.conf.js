13dad32884a3195fd931ef7cd79561ffee3b52ee
Mike Truax <mike@miketruax.com>
2022-12-06 10:54:36 -0600

    moves active to discard on play

--- server/config/socket.conf.js
+++ server/config/socket.conf.js
@@ -1,61 +1,78 @@
 function socketConfig(io) {
-    io.on("connection", (socket) => {
-        const { roomID, username, uid } = socket.handshake.query;
-        let roomCount = parseInt(io.sockets.adapter.rooms.get(roomID)?.size);
-        socket.join(roomID);
+  io.on("connection", (socket) => {
+    const { roomID, username, uid } = socket.handshake.query;
+    let roomCount = parseInt(io.sockets.adapter.rooms.get(roomID)?.size);
+    socket.join(roomID);
 
-        io.to(roomID).emit("user connect", { username, uid });
+    io.to(roomID).emit("user connect", { username, uid });
 
-        io.to(socket.id).emit("host check", roomCount);
+    io.to(socket.id).emit("host check", roomCount);
 
-        socket.on("new message", ({ body }) => {
-            io.to(roomID).emit("new message", { username, body });
-        });
-
-        socket.on(
-            "end turn",
-            ({ players, discardDeck, activeCard, isReverse, turn, playDeck }) => {
-                io.to(roomID).emit("end turn", {
-                    players,
-                    discardDeck,
-                    activeCard,
-                    isReverse,
-                    turn,
-                    playDeck,
-                });
-            }
-        );
-
-        socket.on("start game", ({ players, playDeck, activeCard }) => {
-            io.to(roomID).emit("start game", { players, playDeck, activeCard });
-        });
-
-        socket.on("end game", ({ message }) => {
-            io.to(roomID).emit("end game", { message });
-        });
-
-        socket.on("disconnect", () => {
-            io.to(roomID).emit("user disconnect", { username });
-        });
-
-        socket.on("game active", (activeGame) => {
-            io.socket.broadcast.to(roomID).emit("game active", activeGame);
-        });
-
-        socket.on(
-            "draw card",
-            ({ players, playDeck, turn, draws, activeCard, discardDeck, isReverse }) => {
-                io.to(roomID).emit("draw card", {
-                    players,
-                    playDeck,
-                    turn,
-                    draws,
-                    activeCard,
-                    discardDeck,
-                    isReverse,
-                });
-            }
-        );
+    socket.on("new message", ({ body }) => {
+      io.to(roomID).emit("new message", { username, body });
     });
+
+    socket.on(
+      "end turn",
+      ({
+        players,
+        discardDeck,
+        activeCard,
+        newActiveCard,
+        isReverse,
+        turn,
+        playDeck,
+      }) => {
+        io.to(roomID).emit("end turn", {
+          players,
+          discardDeck,
+          activeCard,
+          newActiveCard,
+          isReverse,
+          turn,
+          playDeck,
+        });
+      }
+    );
+
+    socket.on("start game", ({ players, playDeck, activeCard }) => {
+      io.to(roomID).emit("start game", { players, playDeck, activeCard });
+    });
+
+    socket.on("end game", ({ message }) => {
+      io.to(roomID).emit("end game", { message });
+    });
+
+    socket.on("disconnect", () => {
+      io.to(roomID).emit("user disconnect", { username });
+    });
+
+    socket.on("game active", (activeGame) => {
+      io.socket.broadcast.to(roomID).emit("game active", activeGame);
+    });
+
+    socket.on(
+      "draw card",
+      ({
+        players,
+        playDeck,
+        turn,
+        draws,
+        activeCard,
+        discardDeck,
+        isReverse,
+      }) => {
+        io.to(roomID).emit("draw card", {
+          players,
+          playDeck,
+          turn,
+          draws,
+          activeCard,
+          discardDeck,
+          isReverse,
+        });
+      }
+    );
+  });
 }
 module.exports = socketConfig;
--- src/shared/functions/reshuffleDeck.js
+++ src/shared/functions/reshuffleDeck.js
@@ -1,18 +1,18 @@
-export function reshuffleDeck(discardDeck, activeCard) {
-    let currentIndex = discardDeck.length;
-    let randomIndex;
+export function reshuffleDeck(discardDeck) {
+  let currentIndex = discardDeck.length;
+  let randomIndex;
 
-    while (currentIndex != 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex--;
+  while (currentIndex != 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
 
-        [discardDeck[currentIndex], discardDeck[randomIndex]] = [discardDeck[randomIndex], discardDeck[currentIndex]];
-    }
+    [discardDeck[currentIndex], discardDeck[randomIndex]] = [
+      discardDeck[randomIndex],
+      discardDeck[currentIndex],
+    ];
+  }
 
-    let idx = discardDeck.findIndex((e) => e.color === activeCard.color && e.value === activeCard.value);
-    discardDeck.splice(idx, 1);
-
-    return discardDeck;
+  return discardDeck;
 }
 //remove activeCard from reshuffled play deck
 
