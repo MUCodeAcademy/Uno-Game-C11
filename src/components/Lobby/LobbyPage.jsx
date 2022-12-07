60afc66aff30a51b71161a7e667473813a638f1c
mark waring~ <markwaring00@gmail.com>
2022-12-07 09:08:43 -0600

    adds room search

--- src/components/Lobby/LobbyPage.jsx
+++ src/components/Lobby/LobbyPage.jsx
@@ -11,122 +11,210 @@ import { auth } from "../../firebase.config";
 import React, { useEffect, useMemo, useRef, useState } from "react";
 
 function LobbyPage() {
-    const navigate = useNavigate();
-    const [hasClicked, setHasClicked] = useState(false);
-    const socketRef = useRef(null);
-    const [roomNum, setRoomNum] = useState("");
-    const [rooms, setRooms] = useState([]);
-    useEffect(() => {
-        socketRef.current = io("localhost:8080", {
-            query: {
-                username: auth.currentUser?.displayName,
-            },
-        });
-        socketRef.current.on("rooms", ({ rooms }) => {
-            setRooms(rooms);
-        });
-    }, []);
+  const navigate = useNavigate();
+  const [hasClicked, setHasClicked] = useState(false);
+  const socketRef = useRef(null);
+  const [roomNum, setRoomNum] = useState("");
+  const [roomFilter, setRoomFilter] = useState("");
+  const [rooms, setRooms] = useState([]);
+  useEffect(() => {
+    socketRef.current = io("localhost:8080", {
+      query: {
+        username: auth.currentUser?.displayName,
+      },
+    });
+    socketRef.current.on("rooms", ({ rooms }) => {
+      setRooms(rooms);
+    });
+  }, []);
 
-    const errorMsg = useMemo(() => {
-        if (rooms.includes(roomNum)) return "Room already exists";
-        return "Must be at least 4 characters";
-    }, [roomNum]);
-    return (
-        <>
-            <Container>
-                <Typography variant="h4" textAlign="center" paddingTop={"5px"}>
-                    Join a room
-                </Typography>
-            </Container>
-            <Grid container spacing={1} justifyContent="space-evenly">
-                <Grid
-                    item
-                    xs={12}
-                    sm={5}
-                    style={{
-                        backgroundColor: theme.palette.background.paper,
-                        display: "flex",
-                        flexDirection: "column",
-                        alignItems: "center",
-                        padding: "15px",
-                        margin: "5px 0",
-                    }}
-                >
-                    <Typography variant="h5" textAlign="center">
-                        Create Your Own Game
-                    </Typography>
-                    <form>
-                        <TextField
-                            fullWidth
-                            variant="filled"
-                            id="outlined-required"
-                            label="Room Name"
-                            placeholder="room-name"
-                            color="secondary"
-                            value={roomNum}
-                            helperText={errorMsg}
-                            error={(hasClicked && roomNum.length < 3) || rooms.includes(roomNum)}
-                            onChange={(e) => setRoomNum(e.target.value)}
-                        />
-                        <Button
-                            sx={{ marginTop: "5px" }}
-                            fullWidth
-                            type="submit"
-                            variant="contained"
-                            onClick={(e) => {
-                                e.preventDefault();
-                                setHasClicked(true);
-                                if (roomNum.length > 3 && !rooms.includes(roomNum)) {
-                                    navigate(`/game-room/${roomNum}`);
-                                }
-                            }}
-                        >
-                            Create Room
-                        </Button>
-                    </form>
-                </Grid>
-                <Grid
-                    item
-                    xs={12}
-                    sm={5}
-                    style={{
-                        backgroundColor: theme.palette.background.paper,
-                        display: "flex",
-                        flexDirection: "column",
-                        alignItems: "center",
-                        padding: "15px",
-                        margin: "5px 0",
-                    }}
-                >
-                    <Typography variant="h5" textAlign="center">
-                        Select from active games
-                    </Typography>
+  const errorMsg = useMemo(() => {
+    if (rooms.some((room) => room.id === roomNum)) return "Room already exists";
+    return "Must be at least 4 characters";
+  }, [roomNum]);
 
-                    <Button sx={{ margin: "5px" }} fullWidth variant="contained" size="small" onClick={() => navigate("/game-room/game-1")}>
-                        Game 1
-                    </Button>
-                    <Button sx={{ margin: "5px" }} fullWidth variant="contained" size="small" onClick={() => navigate("/game-room/game-2")}>
-                        Game 2
-                    </Button>
-                    <Button sx={{ margin: "5px" }} fullWidth variant="contained" size="small" onClick={() => navigate("/game-room/game-3")}>
-                        Game 3
-                    </Button>
-                    <Button sx={{ margin: "5px" }} fullWidth variant="contained" size="small" onClick={() => navigate("/game-room/game-4")}>
-                        Game 4
-                    </Button>
-                    {rooms.map((val) => {
-                        let starting = ["game-1", "game-2", "game-3", "game-4"];
-                        if (starting.includes(val)) return <div key={val}></div>;
-                        return (
-                            <Button key={val} sx={{ margin: "5px" }} fullWidth variant="contained" size="small" onClick={() => navigate(`/game-room/${val}`)}>
-                                {val}
-                            </Button>
-                        );
-                    })}
-                </Grid>
-            </Grid>
-        </>
+  const filterErrorMsg = useMemo(() => {
+    const startingVals = ["Game 1", "Game 2", "Game 3", "Game 4"];
+    const isFilteringInit = startingVals.some((val) =>
+      val.toLowerCase().includes(roomFilter.toLowerCase())
     );
+    const doesRoomExist = rooms.some((room) => {
+      return room.id.toLowerCase().includes(roomFilter.toLowerCase());
+    });
+    if (!doesRoomExist && !isFilteringInit && roomFilter.length)
+      return "That room doesn't exist";
+    return null;
+  }, [roomFilter]);
+
+  function buttonDisplay(text) {
+    return {
+      display: `${
+        text.toLowerCase().includes(roomFilter.toLowerCase()) ? "" : "none"
+      }`,
+    };
+  }
+
+  return (
+    <>
+      <Container>
+        <Typography variant="h4" textAlign="center" paddingTop={"5px"}>
+          Join a room
+        </Typography>
+      </Container>
+      <Grid container spacing={1} justifyContent="space-evenly">
+        <Grid
+          item
+          xs={12}
+          sm={5}
+          style={{
+            backgroundColor: theme.palette.background.paper,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            padding: "15px",
+            margin: "5px 0",
+          }}
+        >
+          <Typography variant="h5" textAlign="center">
+            Create Your Own Game
+          </Typography>
+          <form>
+            <TextField
+              fullWidth
+              variant="filled"
+              id="outlined-required"
+              label="Room Name"
+              placeholder="room-name"
+              color="secondary"
+              value={roomNum}
+              helperText={errorMsg}
+              error={
+                (hasClicked && roomNum.length < 4) ||
+                rooms.some((room) => room.id === roomNum)
+              }
+              onChange={(e) => {
+                setHasClicked(false);
+                setRoomNum(e.target.value);
+              }}
+            />
+            <Button
+              sx={{ marginTop: "5px" }}
+              fullWidth
+              type="submit"
+              variant="contained"
+              disabled={errorMsg === "Room already exists"}
+              onClick={(e) => {
+                e.preventDefault();
+                setHasClicked(true);
+                if (
+                  roomNum.length > 3 &&
+                  !rooms.some((room) => room.id.includes(roomNum))
+                ) {
+                  navigate(`/game-room/${roomNum}`);
+                }
+              }}
+            >
+              Create Room
+            </Button>
+          </form>
+        </Grid>
+        <Grid
+          item
+          xs={12}
+          sm={5}
+          style={{
+            backgroundColor: theme.palette.background.paper,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            padding: "15px",
+            margin: "5px 0",
+          }}
+        >
+          <Typography variant="h5" textAlign="center">
+            Select from active games
+          </Typography>
+          <form>
+            <TextField
+              fullWidth
+              variant="filled"
+              id="outlined-required"
+              label="Find a room"
+              placeholder="room-name"
+              color="secondary"
+              value={roomFilter}
+              helperText={filterErrorMsg}
+              error={!!filterErrorMsg}
+              onChange={(e) => setRoomFilter(e.target.value)}
+            />
+          </form>
+          <Button
+            sx={{ margin: "5px" }}
+            fullWidth
+            variant="contained"
+            style={buttonDisplay("Game 1")}
+            size="small"
+            onClick={() => navigate("/game-room/game-1")}
+          >
+            Game 1
+          </Button>
+          <Button
+            sx={{ margin: "5px" }}
+            fullWidth
+            variant="contained"
+            size="small"
+            style={buttonDisplay("Game 2")}
+            onClick={() => navigate("/game-room/game-2")}
+          >
+            Game 2
+          </Button>
+          <Button
+            sx={{ margin: "5px" }}
+            fullWidth
+            variant="contained"
+            size="small"
+            style={buttonDisplay("Game 3")}
+            onClick={() => navigate("/game-room/game-3")}
+          >
+            Game 3
+          </Button>
+          <Button
+            sx={{ margin: "5px" }}
+            fullWidth
+            variant="contained"
+            size="small"
+            style={buttonDisplay("Game 4")}
+            onClick={() => navigate("/game-room/game-4")}
+          >
+            Game 4
+          </Button>
+
+          {rooms
+            .filter((room) => {
+              if (roomFilter.length) {
+                return room.id.includes(roomFilter);
+              } else return room;
+            })
+            .map((val) => {
+              let starting = ["game-1", "game-2", "game-3", "game-4"];
+              if (starting.includes(val.id)) return <div key={val.id}></div>;
+              return (
+                <Button
+                  key={val.id}
+                  sx={{ margin: "5px" }}
+                  fullWidth
+                  variant="contained"
+                  size="small"
+                  onClick={() => navigate(`/game-room/${val.id}`)}
+                >
+                  {val.id}
+                </Button>
+              );
+            })}
+        </Grid>
+      </Grid>
+    </>
+  );
 }
 
 export default LobbyPage;
--- src/shared/hooks/useSocket.js
+++ src/shared/hooks/useSocket.js
@@ -11,237 +11,295 @@ import nextTurn from "../functions/nextTurn";
 //TODO: check for win
 
 const useSocketHook = (roomID, username) => {
-    const {
-        setIsHost,
-        setIsGameActive,
-        setActiveCard,
+  const {
+    setIsHost,
+    setIsGameActive,
+    setActiveCard,
+    players,
+    setPlayers,
+    activeCard,
+    isHost,
+    setPlayDeck,
+    setShuffling,
+
+    setDiscardDeck,
+
+    setIsReverse,
+    setTurn,
+  } = useGameContext();
+  const socketRef = useRef(null);
+  const [messages, setMessages] = useState([]);
+  const [waitingUsers, setWaitingUsers] = useState([]);
+  const [hostuid, setHostuid] = useState();
+  const navigate = useNavigate();
+
+  const initialState = () => {
+    setIsGameActive(false);
+    setPlayDeck([]);
+    setDiscardDeck([]);
+    setActiveCard(null);
+    setIsReverse(false);
+    setShuffling(false);
+    setTurn(0);
+  };
+
+  const waitingToPlayers = () => {
+    // setPlayers([...waitingUsers]);
+    // waitingUsers = [];
+  };
+
+  const playersToWaiting = () => {
+    // waitingUsers = [...players];
+    // waitingUsers.forEach(() => (players.hand = []));
+    // setPlayers([]);
+  };
+
+  const onConnect = (newPlayerName, newPlayerUID) => {
+    console.log("onconnect");
+    let player = { name: "", uid: "", hand: [], isHost: false };
+    player.name = newPlayerName;
+    player.uid = newPlayerUID;
+    if (waitingUsers.length === 0) {
+      player.isHost = true;
+    }
+
+    // waitingUsers.push(player);
+    setPlayers((curr) => [...curr, player]);
+  };
+
+  function onNewGame() {
+    waitingToPlayers();
+    setTurn(Math.random(Math.floor() * players.length - 1));
+    // setIsGameActive(true);
+  }
+
+  const onDisconnect = (player) => {
+    if (player.isHost) {
+      endGame();
+    }
+    let cardsToDiscard = [...player.hand];
+    setDiscardDeck((curr) => [...curr, cardsToDiscard]);
+  };
+
+  useEffect(() => {
+    socketRef.current = io("localhost:8080", {
+      query: {
+        username,
+        roomID,
+        uid: auth.currentUser?.uid,
+      },
+    });
+
+    socketRef.current.on("host check", ({ roomCount, uid }) => {
+      if (roomCount === null) {
+        setIsHost(true);
+        socketRef.current.emit("sendhostuid", uid);
+      }
+    });
+
+    socketRef.current.on("game active", (gameActive) => {
+      setIsGameActive(gameActive);
+    });
+
+    socketRef.current.on(
+      "draw card",
+      ({
         players,
-        setPlayers,
+        playDeck,
+        turn,
+        draws,
         activeCard,
-        isHost,
-        setPlayDeck,
-        setShuffling,
+        discardDeck,
+        isReverse,
+      }) => {
+        let cards = playDeck.splice(0, draws);
+        setPlayDeck(playDeck);
+        setDiscardDeck(discardDeck);
+        players[turn].hand = [...players[turn].hand, ...cards];
+        setPlayers(players);
+      }
+    );
 
-        setDiscardDeck,
-
-        setIsReverse,
-        setTurn,
-    } = useGameContext();
-    const socketRef = useRef(null);
-    const [messages, setMessages] = useState([]);
-    const [waitingUsers, setWaitingUsers] = useState([]);
-    const [hostuid, setHostuid] = useState();
-    const navigate = useNavigate();
-
-    const initialState = () => {
-        setIsGameActive(false);
-        setPlayDeck([]);
-        setDiscardDeck([]);
-        setActiveCard(null);
-        setIsReverse(false);
-        setShuffling(false);
-        setTurn(0);
-    };
-
-    const waitingToPlayers = () => {
-        // setPlayers([...waitingUsers]);
-        // waitingUsers = [];
-    };
-
-    const playersToWaiting = () => {
-        // waitingUsers = [...players];
-        // waitingUsers.forEach(() => (players.hand = []));
-        // setPlayers([]);
-    };
-
-    const onConnect = (newPlayerName, newPlayerUID) => {
-        let player = { name: "", uid: "", hand: [], isHost: false };
-        player.name = newPlayerName;
-        player.uid = newPlayerUID;
-        if (waitingUsers.length === 0) {
-            player.isHost = true;
+    socketRef.current.on(
+      "end turn",
+      ({
+        players,
+        discardDeck,
+        newActiveCard,
+        activeCard,
+        isReverse,
+        turn,
+        playDeck,
+      }) => {
+        const message = checkForEndGame(players, playDeck, discardDeck);
+        //message will be null if end game conditions are not met
+        if (message) {
+          endGame(message);
+          return;
         }
 
-        // waitingUsers.push(player);
-        setPlayers((curr) => [...curr, player]);
-    };
-
-    function onNewGame() {
-        waitingToPlayers();
-        setTurn(Math.random(Math.floor() * players.length - 1));
-        // setIsGameActive(true);
-    }
-
-    const onDisconnect = (player) => {
-        if (player.isHost) {
-            endGame();
+        if (
+          activeCard.value === CardValue.Wild ||
+          activeCard.value === CardValue.WildDrawFour
+        ) {
+          activeCard.color = CardColor.Black;
         }
-        let cardsToDiscard = [...player.hand];
-        setDiscardDeck((curr) => [...curr, cardsToDiscard]);
-    };
-
-    useEffect(() => {
-        socketRef.current = io("10.200.224.164:8080", {
-            query: {
-                username,
-                roomID,
-                uid: auth.currentUser?.uid,
-            },
-        });
-
-        socketRef.current.on("host check", ({ roomCount, uid }) => {
-            if (roomCount === null) {
-                setIsHost(true);
-                socketRef.current.emit("sendhostuid", uid);
-            }
-        });
-
-        socketRef.current.on("game active", (gameActive) => {
-            setIsGameActive(gameActive);
-        });
-
-        socketRef.current.on("draw card", ({ players, playDeck, turn, draws, activeCard, discardDeck, isReverse }) => {
-            let cards = playDeck.splice(0, draws);
-            setPlayDeck(playDeck);
-            setDiscardDeck(discardDeck);
-            players[turn].hand = [...players[turn].hand, ...cards];
-            setPlayers(players);
-        });
-
-        socketRef.current.on("end turn", ({ players, discardDeck, newActiveCard, activeCard, isReverse, turn, playDeck }) => {
-            const message = checkForEndGame(players, playDeck, discardDeck);
-            //message will be null if end game conditions are not met
-            if (message) {
-                endGame(message);
-                return;
-            }
-
-            if (activeCard.value === CardValue.Wild || activeCard.value === CardValue.WildDrawFour) {
-                activeCard.color = CardColor.Black;
-            }
-            setDiscardDeck([...discardDeck, activeCard]);
-            setActiveCard(newActiveCard);
-            setIsReverse(isReverse);
-            setPlayers(players);
-            const { next, skipped } = nextTurn(turn, isReverse, players, newActiveCard);
-            if (newActiveCard.value === CardValue.DrawTwo || newActiveCard.value === CardValue.WildDrawFour || newActiveCard.value === CardValue.Skip) {
-                setTurn(skipped);
-            } else {
-                setTurn(next);
-            }
-            if (newActiveCard.value === CardValue.DrawTwo || newActiveCard.value === CardValue.WildDrawFour) {
-                const draw = newActiveCard.value === CardValue.DrawTwo ? 2 : 4;
-                drawCard(players, playDeck, next, draw, isReverse);
-            }
-        });
-        socketRef.current.on("user connect", ({ username, uid }) => {
-            setMessages((curr) => [...curr, { body: `${username} has connected` }]);
-            onConnect(username, uid);
-        });
-
-        socketRef.current.on("start game", ({ players, playDeck, activeCard }) => {
-            setPlayers(players);
-            setPlayDeck(playDeck);
-            setActiveCard(activeCard);
-            setDiscardDeck([]);
-            setTurn(0);
-            setIsGameActive(true);
-            //! onNewGame();
-        });
-
-        socketRef.current.on("new message", (msg) => {
-            setMessages((curr) => [...curr, msg]);
-        });
-        socketRef.current.on("user disconnect", ({ username, uid }) => {
-            setPlayers((curr) => {
-                //Check for player to remove
-                let playerIndex = curr.findIndex((p) => p.uid === uid);
-                let playerToRemove = curr[playerIndex];
-                curr.splice(playerIndex, 1);
-                console.log(curr);
-                // Get their cards
-                // Update turn order
-                setTurn((currTurn) => (currTurn >= playerIndex ? currTurn - 1 : currTurn));
-                setDiscardDeck((curr) => [...curr, ...playerToRemove.hand]);
-                return [...curr];
-            });
-            setMessages((curr) => [...curr, { body: `${username} has disconnected` }]);
-            // onDisconnect();
-        });
-        socketRef.current.on("host disconnected", () => {
-            navigate("/");
-        });
-
-        socketRef.current.on("end game", ({ message }) => {
-            setMessages((curr) => [...curr, { body: message }]);
-            playersToWaiting();
-            initialState();
-            setIsGameActive(false);
-        });
-
-        return () => socketRef.current?.disconnect();
-    }, [roomID, username]);
-
-    function sendMessage(body) {
-        socketRef.current.emit("new message", { body });
-    }
-
-    function startGame(newDeck, newPlayers, gameStartCard) {
-        socketRef.current.emit("start game", {
-            players: newPlayers,
-            playDeck: newDeck,
-            activeCard: gameStartCard,
-        });
-    }
-
-    function endGame(message) {
-        socketRef.current.emit("end game", {
-            message,
-        });
-    }
-
-    function endTurn(players, discardDeck, newActiveCard, isReverse, turn, playDeck) {
-        socketRef.current.emit("end turn", {
-            activeCard,
-            players,
-            discardDeck,
-            newActiveCard,
-            isReverse,
-            turn,
-            playDeck,
-        });
-    }
-
-    function drawCard(players, playDeck, turn, draws, activeCard, discardDeck, isReverse) {
-        if (playDeck.length + discardDeck.length < draws) {
-            endGame("Stalemate, not enough cards to draw.");
-            return;
+        setDiscardDeck([...discardDeck, activeCard]);
+        setActiveCard(newActiveCard);
+        setIsReverse(isReverse);
+        setPlayers(players);
+        const { next, skipped } = nextTurn(
+          turn,
+          isReverse,
+          players,
+          newActiveCard
+        );
+        if (
+          newActiveCard.value === CardValue.DrawTwo ||
+          newActiveCard.value === CardValue.WildDrawFour ||
+          newActiveCard.value === CardValue.Skip
+        ) {
+          setTurn(skipped);
+        } else {
+          setTurn(next);
         }
-
-        if (playDeck.length < draws) {
-            const deck = shuffleDeck(discardDeck);
-            playDeck = [...playDeck, ...deck];
-            discardDeck = [];
+        if (
+          newActiveCard.value === CardValue.DrawTwo ||
+          newActiveCard.value === CardValue.WildDrawFour
+        ) {
+          const draw = newActiveCard.value === CardValue.DrawTwo ? 2 : 4;
+          drawCard(players, playDeck, next, draw, isReverse);
         }
-        socketRef.current.emit("draw card", {
-            players,
-            playDeck,
-            turn,
-            draws,
-            activeCard,
-            discardDeck,
-            isReverse,
-        });
+      }
+    );
+    socketRef.current.on("user connect", ({ username, uid }) => {
+      setMessages((curr) => [...curr, { body: `${username} has connected` }]);
+      onConnect(username, uid);
+    });
+
+    socketRef.current.on("start game", ({ players, playDeck, activeCard }) => {
+      setPlayers(players);
+      setPlayDeck(playDeck);
+      setActiveCard(activeCard);
+      setDiscardDeck([]);
+      setTurn(0);
+      setIsGameActive(true);
+      //! onNewGame();
+    });
+
+    socketRef.current.on("new message", (msg) => {
+      setMessages((curr) => [...curr, msg]);
+    });
+    socketRef.current.on("user disconnect", ({ username, uid }) => {
+      setPlayers((curr) => {
+        //Check for player to remove
+        let playerIndex = curr.findIndex((p) => p.uid === uid);
+        let playerToRemove = curr[playerIndex];
+        curr.splice(playerIndex, 1);
+        console.log(curr);
+        // Get their cards
+        // Update turn order
+        setTurn((currTurn) =>
+          currTurn >= playerIndex ? currTurn - 1 : currTurn
+        );
+        setDiscardDeck((curr) => [...curr, ...playerToRemove.hand]);
+        return [...curr];
+      });
+      setMessages((curr) => [
+        ...curr,
+        { body: `${username} has disconnected` },
+      ]);
+      // onDisconnect();
+    });
+    socketRef.current.on("host disconnected", () => {
+      navigate("/");
+    });
+
+    socketRef.current.on("end game", ({ message }) => {
+      setMessages((curr) => [...curr, { body: message }]);
+      playersToWaiting();
+      initialState();
+      setIsGameActive(false);
+    });
+
+    return () => socketRef.current?.disconnect();
+  }, [roomID, username]);
+
+  function sendMessage(body) {
+    socketRef.current.emit("new message", { body });
+  }
+
+  function startGame(newDeck, newPlayers, gameStartCard) {
+    socketRef.current.emit("start game", {
+      players: newPlayers,
+      playDeck: newDeck,
+      activeCard: gameStartCard,
+    });
+  }
+
+  function endGame(message) {
+    socketRef.current.emit("end game", {
+      message,
+    });
+  }
+
+  function endTurn(
+    players,
+    discardDeck,
+    newActiveCard,
+    isReverse,
+    turn,
+    playDeck
+  ) {
+    socketRef.current.emit("end turn", {
+      activeCard,
+      players,
+      discardDeck,
+      newActiveCard,
+      isReverse,
+      turn,
+      playDeck,
+    });
+  }
+
+  function drawCard(
+    players,
+    playDeck,
+    turn,
+    draws,
+    activeCard,
+    discardDeck,
+    isReverse
+  ) {
+    if (playDeck.length + discardDeck.length < draws) {
+      endGame("Stalemate, not enough cards to draw.");
+      return;
     }
 
-    return {
-        messages,
-        sendMessage,
-        endGame,
-        endTurn,
-        drawCard,
-        startGame,
-    };
+    if (playDeck.length < draws) {
+      const deck = shuffleDeck(discardDeck);
+      playDeck = [...playDeck, ...deck];
+      discardDeck = [];
+    }
+    socketRef.current.emit("draw card", {
+      players,
+      playDeck,
+      turn,
+      draws,
+      activeCard,
+      discardDeck,
+      isReverse,
+    });
+  }
+
+  return {
+    messages,
+    sendMessage,
+    endGame,
+    endTurn,
+    drawCard,
+    startGame,
+  };
 };
 
 export default useSocketHook;
