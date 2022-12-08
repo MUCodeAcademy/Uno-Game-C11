fa647dfad6db9b87e0b51ba03fc102b567f6bc89
gabebierman <gabebierman@gmail.com>
2022-12-08 15:43:18 -0600

    implement table in leaderboard

--- src/components/Leaderboard/LeaderboardPage.jsx
+++ src/components/Leaderboard/LeaderboardPage.jsx
@@ -1,116 +1,117 @@
 import React, { useEffect, useState, useMemo } from "react";
-import Typography from "@mui/material/Typography";
-import Grid from "@mui/material/Grid";
-import Container from "@mui/material/Container";
 import { theme } from "../../shared/styled/themes/Theme";
 import { ref, child, get } from "firebase/database";
 import { database } from "../../firebase.config";
 import { auth } from "../../firebase.config";
+import { Container, Grid, Typography, Table, TableContainer } from "@mui/material";
+import { EnhancedTable, EnhancedTableHead } from "./LeaderboardTable";
 
 function LeaderBoardPage() {
-  const [leaderBoard, setLeaderBoard] = useState([]);
-  const dbRef = ref(database);
+    const [leaderBoard, setLeaderBoard] = useState([]);
+    const dbRef = ref(database);
 
-  async function getDB() {
-    try {
-      await get(child(dbRef, `users`)).then((snapshot) => {
-        if (snapshot.exists()) {
-          let data = snapshot.val();
-          setLeaderBoard(putJSONinArray(data));
-        } else {
-          console.log("snapshot doesn't exist");
+    async function getDB() {
+        try {
+            await get(child(dbRef, `users`)).then((snapshot) => {
+                if (snapshot.exists()) {
+                    let data = snapshot.val();
+                    setLeaderBoard(putJSONinArray(data));
+                } else {
+                    console.log("snapshot doesn't exist");
+                }
+            });
+        } catch (err) {
+            console.error(err);
         }
-      });
-    } catch (err) {
-      console.error(err);
     }
-  }
 
-  function putJSONinArray(data) {
-    let arr = [];
-    for (let user in data) {
-      const played = parseInt(data[user]["total games played"]);
-      const won = parseInt(data[user]["total games won"]);
-      const lost = parseInt(data[user]["total games lost"]);
-      const pct = played > 0 ? won / played : 0;
-      arr.push({
-        uid: user,
-        name: data[user]["name"],
-        played: played,
-        won: won,
-        lost: lost,
-        pct: pct,
-      });
+    function putJSONinArray(data) {
+        let arr = [];
+        for (let user in data) {
+            const played = parseInt(data[user]["total games played"]);
+            const won = parseInt(data[user]["total games won"]);
+            const lost = parseInt(data[user]["total games lost"]);
+            const drawn = parseInt(data[user]["total games drawn"]);
+            const pct = played > 0 ? Math.floor((won / played) * 10000) / 100 : 0;
+            arr.push({
+                uid: user,
+                name: data[user]["name"],
+                played: played,
+                won: won,
+                lost: lost,
+                pct: pct,
+                drawn: drawn,
+            });
+        }
+        arr.sort((a, b) => b.won - a.won);
+        return arr;
     }
-    arr.sort((a, b) => b.won - a.won);
-    return arr;
-  }
-  console.log(leaderBoard);
-  useEffect(() => {
-    async function getData() {
-      await getDB();
-    }
-    getData();
-  }, []);
+    console.log(leaderBoard);
+    useEffect(() => {
+        async function getData() {
+            await getDB();
+        }
+        getData();
+    }, []);
 
-  const personalStats = useMemo(
-    () => leaderBoard.find((u) => u.uid === auth.currentUser?.uid),
-    [leaderBoard, auth.currentUser?.uid]
-  );
+    const personalStats = useMemo(
+        () => leaderBoard.find((u) => u.uid === auth.currentUser?.uid),
+        [leaderBoard, auth.currentUser?.uid]
+    );
 
-  console.log(personalStats);
-  return (
-    <div>
-      <Container style={{ margin: "10px 0px" }}>
-        <Typography
-          style={{ margin: "10px", padding: "15px" }}
-          variant="h4"
-          textAlign="center"
-        >
-          Game Statistics
-        </Typography>
-        <Grid container spacing={1} justifyContent="center">
-          <Grid
-            item
-            xs={12}
-            style={{
-              backgroundColor: theme.palette.background.paper,
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-              padding: "15px",
-              margin: "5px 0",
-              color: "white",
-            }}
-          >
-            <Typography variant="h5" textAlign="center">
-              Your Stats
-            </Typography>
-            <div></div>
-          </Grid>
+    console.log(personalStats);
+    return (
+        <div>
+            <Container style={{ margin: "10px 0px" }}>
+                <Typography
+                    style={{ margin: "10px", padding: "15px" }}
+                    variant="h4"
+                    textAlign="center"
+                >
+                    Game Statistics
+                </Typography>
+                <Grid container spacing={1} justifyContent="center">
+                    <Grid
+                        item
+                        xs={12}
+                        style={{
+                            backgroundColor: theme.palette.background.paper,
+                            display: "flex",
+                            flexDirection: "column",
+                            alignItems: "center",
+                            padding: "15px",
+                            margin: "5px 0",
+                            color: "white",
+                        }}
+                    >
+                        <Typography variant="h5" textAlign="center">
+                            Your Stats
+                        </Typography>
+                        <div></div>
+                    </Grid>
 
-          <Grid
-            item
-            xs={12}
-            style={{
-              backgroundColor: theme.palette.background.paper,
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-              padding: "15px",
-              margin: "5px 0",
-              color: "white",
-            }}
-          >
-            <Typography variant="h5" textAlign="center">
-              Leader Board
-            </Typography>
-            <div></div>
-          </Grid>
-        </Grid>
-      </Container>
-    </div>
-  );
+                    <Grid
+                        item
+                        xs={12}
+                        style={{
+                            backgroundColor: theme.palette.background.paper,
+                            display: "flex",
+                            flexDirection: "column",
+                            alignItems: "center",
+                            padding: "15px",
+                            margin: "5px 0",
+                            color: "white",
+                        }}
+                    >
+                        <Typography variant="h5" textAlign="center">
+                            Leader Board
+                        </Typography>
+                        <EnhancedTable leaderBoard={leaderBoard}></EnhancedTable>
+                    </Grid>
+                </Grid>
+            </Container>
+        </div>
+    );
 }
 
 export default LeaderBoardPage;
