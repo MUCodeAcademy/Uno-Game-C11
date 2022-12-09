1d4a29ae2d5a61348f32ef173ce4d47a5d53ceb2
gabebierman <gabebierman@gmail.com>
2022-12-09 10:01:59 -0600

    start server board

---
+++ src/components/Leaderboard/tables/ServerTable.jsx
@@ -0,0 +1,116 @@
+import React, { useMemo } from "react";
+import {
+    Box,
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TableRow,
+    TableSortLabel,
+    Paper,
+} from "@mui/material";
+import { visuallyHidden } from "@mui/utils";
+
+const headCells = [
+    {
+        id: "gamesstarted",
+        numeric: false,
+        disablePadding: true,
+        label: "Games Started",
+    },
+    {
+        id: "gamescompleted",
+        numeric: true,
+        disablePadding: false,
+        label: "Games Completed",
+    },
+    {
+        id: "gamesabandonded",
+        numeric: true,
+        disablePadding: false,
+        label: "Games Abandoned",
+    },
+    {
+        id: "gamesdrawn",
+        numeric: true,
+        disablePadding: false,
+        label: "Total Games Won",
+    },
+];
+
+export function EnhancedTableHead(props) {
+    const { order, orderBy } = props;
+
+    return (
+        <TableHead>
+            <TableRow>
+                {headCells.map((headCell, index) => (
+                    <TableCell
+                        key={index}
+                        align={headCell.numeric ? "right" : "left"}
+                        padding={headCell.disablePadding ? "none" : "normal"}
+                    >
+                        <TableSortLabel>
+                            {headCell.label}
+                            {orderBy === headCell.id ? (
+                                <Box component="span" sx={visuallyHidden}>
+                                    {order === "desc"
+                                        ? "sorted descending"
+                                        : "sorted ascending"}
+                                </Box>
+                            ) : null}
+                        </TableSortLabel>
+                    </TableCell>
+                ))}
+            </TableRow>
+        </TableHead>
+    );
+}
+
+//!
+//!
+
+export function ServerTable({ leaderBoard }) {
+    return (
+        <>
+            {personalStats && (
+                <Box sx={{ width: "100%" }}>
+                    <Paper sx={{ width: "100%", mb: 2 }}>
+                        <TableContainer>
+                            <Table
+                                sx={{ minWidth: 750 }}
+                                aria-labelledby="tableTitle"
+                                size={"medium"}
+                            >
+                                <EnhancedTableHead rowCount={1} />
+                                <TableBody>
+                                    <TableRow hover role="checkbox" tabIndex={-1}>
+                                        <TableCell component="th" scope="row" padding="none">
+                                            {personalStats.name}
+                                        </TableCell>
+                                        <TableCell align="right">
+                                            {personalStats.pct}%
+                                        </TableCell>
+                                        <TableCell align="right">
+                                            {personalStats.played}
+                                        </TableCell>
+                                        <TableCell align="right">
+                                            {personalStats.won}
+                                        </TableCell>
+                                        <TableCell align="right">
+                                            {personalStats.lost}
+                                        </TableCell>
+                                        <TableCell align="right">
+                                            {personalStats.drawn}
+                                        </TableCell>
+                                    </TableRow>
+                                </TableBody>
+                            </Table>
+                        </TableContainer>
+                    </Paper>
+                </Box>
+            )}
+        </>
+    );
+}
